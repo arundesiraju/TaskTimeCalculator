595f42c6308d5e310d929a593cb705d216e4849e
Arun Desiraju <39387479+arundesiraju@users.noreply.github.com>
2018-09-17 22:22:56 -0700

    complete calculate start, stop and duration functions

--- TaskTimeCalculator/TaskTimeCalculator/TaskTimeCalculator.cpp
+++ TaskTimeCalculator/TaskTimeCalculator/TaskTimeCalculator.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <string>
 #include <map>
+#include <Windows.h>
 
 using namespace std;
 
 int InitializeTaskTimer(string uniquetimerName, int *uniquetimerID);
-int StartTaskTimer(int uniquetimerID);
-int StopTaskTimer(int uniquetimerID);
+int StartTaskTimer(string uniquetimerName, int uniquetimerID);
+int StopTaskTimer(string uniquetimerName, int uniquetimerID);
+std::chrono::duration<double> CalculateTaskTime(string uniquetimerName, int uniquetimerID);
 
 void TaskA();
 void TaskB();
 void TaskC();
 
 map<string, int> g_timerMap;
+map<string, chrono::time_point<chrono::system_clock>> g_start_times;
+map<string, chrono::time_point<chrono::system_clock>> g_stop_times;
+map<string, std::chrono::duration<double>> g_durations;
+
 int g_timerID;
 
 int main()
@@ -34,7 +40,13 @@ void TaskA()
     int timerID = 0;
     InitializeTaskTimer("TaskA", &timerID);
 
-    cout << "TaskA" << endl;
+    StartTaskTimer("TaskA", timerID);
+
+    Sleep(1513);
+
+    StopTaskTimer("TaskA", timerID);
+    
+    std::chrono::duration<double> taskA_time = CalculateTaskTime("TaskA", timerID);
 }
 
 void TaskB()
@@ -42,7 +54,13 @@ void TaskB()
     int timerID = 0;
     InitializeTaskTimer("TaskB", &timerID);
 
-    cout << "TaskB" << endl;
+    StartTaskTimer("TaskB", timerID);
+
+    Sleep(2124);
+
+    StopTaskTimer("TaskB", timerID);
+    
+    std::chrono::duration<double> taskA_time = CalculateTaskTime("TaskB", timerID);
 }
 
 void TaskC()
@@ -50,8 +68,15 @@ void TaskC()
     int timerID = 0;
     InitializeTaskTimer("TaskC", &timerID);
 
-    cout << "TaskC" << endl;
+    StartTaskTimer("TaskC", timerID);
+
+    Sleep(3754);
+
+    StopTaskTimer("TaskC", timerID);
+    
+    std::chrono::duration<double> taskA_time = CalculateTaskTime("TaskC", timerID);
 }
+
 
 int InitializeTaskTimer(string uniquetimerName, int *uniquetimerID)
 {
@@ -68,4 +93,38 @@ int InitializeTaskTimer(string uniquetimerName, int *uniquetimerID)
     }
    
     return 0;
+}
+
+int StartTaskTimer (string uniquetimerName, int uniquetimerID)
+{
+    chrono::time_point<chrono::system_clock> start_time;
+
+    start_time = chrono::system_clock::now();
+
+    g_start_times.insert(pair<string, chrono::time_point<chrono::system_clock>>(uniquetimerName, start_time));
+
+    return 0;
+}
+
+int StopTaskTimer (string uniquetimerName, int uniquetimerID)
+{
+    chrono::time_point<chrono::system_clock> stop_time;
+
+    stop_time = chrono::system_clock::now();
+
+    g_stop_times.insert(pair<string, chrono::time_point<chrono::system_clock>>(uniquetimerName, stop_time));
+
+    return 0;
+}
+
+std::chrono::duration<double> CalculateTaskTime(string uniquetimerName, int uniquetimerID)
+{
+    chrono::time_point<chrono::system_clock> diff_start_time = g_start_times.find(uniquetimerName)->second;
+    chrono::time_point<chrono::system_clock> diff_stop_time = g_stop_times.find(uniquetimerName)->second;
+
+    std::chrono::duration<double> diff_time = diff_stop_time - diff_start_time;
+
+    g_durations.insert(pair<string, std::chrono::duration<double>>(uniquetimerName, diff_time));
+
+    return diff_time;
 }
